bcd362bcb193b04ab000b7f6bc3efbe5e3e4977d
pranay <pranay.prabhu.777@gmail.com>
2026-09-21 20:15:22 +0530

    comiiting listand tuples

---
+++ Python_coding/List_coding.py
@@ -0,0 +1,19 @@
+# find maximum Without using max()
+'''
+numbers = list(map(int, input("Enter numbers: ").split()))
+max_number = numbers[0]
+for num in numbers:
+    if num > max_number:
+        max_number = num
+
+
+print("Maximum number:", max_number)
+'''
+
+# find the total and average in the list 
+
+numbers = list(map(int,input("Enter numbers: ").split()))
+total = sum(numbers)
+average = total / len(numbers)
+print("Total:", total)
+print("Average:", average)
